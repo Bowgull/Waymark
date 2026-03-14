ff84edc3d0025b2065e46843810776a2be5798a0
Bowgull <Bowgull@users.noreply.github.com>
2026-03-14 14:46:57 -0400

    add capacitor and iOS app shell

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.joshbocas.app',
+  appName: 'waymark-c3',
+  webDir: 'dist'
+};
+
+export default config;
